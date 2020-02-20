dae8441eafa6876175d8992d59769a5017d44e5b
samuelvarejames <samuelvarejames@gmail.com>
2020-02-20 02:36:45 +0100

    Rough sketch for heapify function

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Define the tree")
+
+	arr := []int{2, 3, 4, 7, 5, 8, 9, 8, 10, 12, 11, 13, 8}
+
+	constructHeap(arr)
+}
+
+type node struct {
+	key   int
+	left  interface{}
+	right interface{}
+}
+
+func constructHeap(arr []int) {
+
+	for i := range arr {
+		heapify(i, arr)
+	}
+}
+
+func heapify(i int, arr []int) {
+
+	var rootNode node
+
+	left := 2*i + 1
+	right := 2*i + 2
+
+	rootNode = node{key: arr[i]}
+	rootNode.left = node{key: arr[left]}
+	rootNode.right = node{key: arr[right]}
+}
