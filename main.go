b592d299accf5a9dbbfcb578ec3c57b251024ffd
samuelvarejames <samuelvarejames@gmail.com>
2020-02-20 10:47:18 +0100

    Use pointers

--- main.go
+++ main.go
@@ -6,39 +6,38 @@ func main() {
 	fmt.Println("Define the tree")
 
 	arr := []int{2, 3, 4, 7, 5, 8, 9, 8, 10, 12, 11, 13, 8}
-	var root interface{}
+	var root *node
 
 	l := len(arr)
-	// root := interface{}(nil)
+	root = &node{}
 
-	root = constructBTree(arr, root, 0, l)
-	fmt.Println(root)
+	result := constructBinTree(arr, root, 0, l)
+	fmt.Println(result)
 
 }
 
 type node struct {
 	key   int
-	left  interface{}
-	right interface{}
+	left  *node
+	right *node
 }
 
 // let's construct a simple Binary tree
-func constructBTree(arr []int, root interface{}, index int, l int) interface{} {
+func constructBinTree(arr []int, root *node, index int, l int) node {
 
 	var leftIndex int
 	var rigntIndex int
 
 	// base condition
 	if index < l {
-		n := node{key: arr[index]}
-		root := n
+		n := &node{key: arr[index]}
+		root = n
 
 		leftIndex = 2*index + 1
 		rigntIndex = 2*index + 2
 
-		constructBTree(arr, root.left, leftIndex, l)
-		constructBTree(arr, root.right, rigntIndex, l)
+		constructBinTree(arr, root.left, leftIndex, l)
+		constructBinTree(arr, root.right, rigntIndex, l)
 	}
-
-	return root
+	return *root
 }
