66a6f3230a1c9fbd41284453f70d47f00206452d
samuelvarejames <samuelvarejames@gmail.com>
2020-02-25 23:41:32 +0100

    Fix bug

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import "fmt"
 func main() {
 	fmt.Println("Define the array")
 
-	arrOne := []int{2, 3, 4, 7, 5, 8, 9, 8, 10, 12, 11, 13, 8} // Binary Array
-	// heapArr := []int{4, 7, 8, 3, 2, 6, 5, 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17 } // Non Heap Array
+	arrOne := []int{2, 3, 4, 7, 5, 8, 9, 8, 10, 12, 11, 13, 8}                 // Binary Array
+	heapArr := []int{4, 7, 8, 3, 2, 6, 5, 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17} // Non Heap Array
+
+	buildHeap(heapArr, len(heapArr))
+	printHeap(heapArr, len(heapArr))
 
 	fmt.Println(arrOne)
 
@@ -31,8 +34,8 @@ type node struct {
 }
 
 // To heapify a subtree rooted with node i (rootIndex)
-// which is an index in arr[]. arrayLength is the size of the heap.
-func heapify(arr []int, rootIndex, arrayLength int) {
+// which is an index in arr[]. size is the size of the heap.
+func heapify(arr []int, rootIndex, size int) {
 	var leftIndex int
 	var rightIndex int
 
@@ -44,12 +47,12 @@ func heapify(arr []int, rootIndex, arrayLength int) {
 	rightIndex = 2*rootIndex + 2
 
 	// if the left child is larger than root
-	if leftIndex <= arrayLength && arr[leftIndex] > arr[rootIndex] {
+	if leftIndex < size && arr[leftIndex] > arr[largest] {
 		largest = leftIndex
 	}
 
 	// if the right child is larger than largest so far
-	if rightIndex <= arrayLength && arr[rightIndex] > arr[largest] {
+	if rightIndex < size && arr[rightIndex] > arr[largest] {
 		largest = rightIndex
 	}
 
@@ -58,7 +61,7 @@ func heapify(arr []int, rootIndex, arrayLength int) {
 		swap(arr, rootIndex, largest)
 
 		// recursively heapify the affected sub-tree
-		heapify(arr, largest, arrayLength)
+		heapify(arr, largest, size)
 	}
 }
 
@@ -66,7 +69,6 @@ func buildHeap(arr []int, size int) {
 
 	// Index of the last non-leaf node
 	startIdx := (size / 2) - 1
-	fmt.Println(startIdx, "startIdx haq haq haq")
 
 	// Perform reverse level order traversal
 	// from last non-leaf node and heapify
@@ -78,9 +80,12 @@ func buildHeap(arr []int, size int) {
 }
 
 func printHeap(arr []int, size int) {
+	result := make([]int, 0)
 	for _, val := range arr {
-		fmt.Println(val)
+		result = append(result, val)
 	}
+
+	fmt.Println(result)
 }
 
 func swap(arr []int, x, y int) {
