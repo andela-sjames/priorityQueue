5b853dcdce4a69a64f69bf5e509fccef2bc8fd38
samuelvarejames <samuelvarejames@gmail.com>
2020-02-26 00:03:32 +0100

    Clean up

--- main.go
+++ main.go
@@ -5,26 +5,10 @@ import "fmt"
 func main() {
 	fmt.Println("Define the array")
 
-	arrOne := []int{2, 3, 4, 7, 5, 8, 9, 8, 10, 12, 11, 13, 8}                 // Binary Array
 	heapArr := []int{4, 7, 8, 3, 2, 6, 5, 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17} // Non Heap Array
 
 	buildHeap(heapArr, len(heapArr))
 	printHeap(heapArr, len(heapArr))
-
-	fmt.Println(arrOne)
-
-	l := len(arrOne)
-	root := &node{}
-
-	s := make([]int, 0)
-
-	fmt.Println("Construct the Binary tree")
-	tree := constructBinTree(arrOne, root, 0, l)
-
-	fmt.Println("preOrderTraverse the Binary tree")
-	result := preOrderTraverse(tree, s)
-
-	fmt.Println(result)
 }
 
 type node struct {
@@ -93,37 +77,4 @@ func swap(arr []int, x, y int) {
 	tmp := arr[x]
 	arr[x] = arr[y]
 	arr[y] = tmp
-}
-
-// construct a simple Binary tree
-func constructBinTree(arr []int, root *node, index int, l int) *node {
-
-	var leftIndex int
-	var rightIndex int
-
-	// base condition
-	if index < l {
-		n := &node{key: arr[index]}
-		root = n
-
-		leftIndex = 2*index + 1
-		rightIndex = 2*index + 2
-
-		// recursive steps
-		root.left = constructBinTree(arr, root.left, leftIndex, l)
-		root.right = constructBinTree(arr, root.right, rightIndex, l)
-	}
-	return root
-}
-
-// Depth first search (DFS) Pre-Order traversal
-func preOrderTraverse(tree *node, array []int) []int {
-
-	if tree != nil {
-		array = append(array, tree.key)             // root
-		array = preOrderTraverse(tree.left, array)  // left
-		array = preOrderTraverse(tree.right, array) // right
-	}
-
-	return array
 }
