2e3ec9237ef96ddaec439d6c45745b73515fbb15
samuelvarejames <samuelvarejames@gmail.com>
2020-03-05 02:17:37 +0100

    Define GetFromTable function

--- pqueue/hash.go
+++ pqueue/hash.go
@@ -6,32 +6,57 @@ import (
 
 type htable map[int][]int
 
+// Pop from queue
+// x, a = a[0], a[1:]
+
+// Pop from stack
+// x, a = a[len(a)-1], a[:len(a)-1]
+
+// Push
+// a = append(a, x)
+
 // HashTable struct defined
-var hashTable = struct {
+var HashTable = struct {
 	sync.RWMutex
 	m htable
 }{m: make(htable)}
 
 // AddToTable struct defined
+// If the value does not exist, add an new index
+// to the array and make a new key, pair
+// otherwise, get the exiting value(array),
+// update it (to avoid collision) and update the key, pair
+// i.e. key = updated_array_value
 func AddToTable(priority, index int) {
 
-	/**
-	If the value does not exist, add an new index
-	to the array and make a new key, pair
-	otherwise, get the exiting value(array),
-	update it (to avoid collision) and update the key, pair
-	i.e. key = updated_array_value
-	*/
 	arr := make([]int, 0)
 
-	hashTable.Lock()
-	defer hashTable.Unlock()
+	HashTable.Lock()
+	defer HashTable.Unlock()
 
-	if val, exist := hashTable.m[priority]; !exist {
+	if val, exist := HashTable.m[priority]; !exist {
 		arr = append(arr, index)
-		hashTable.m[priority] = arr
+		HashTable.m[priority] = arr
 	} else {
 		val = append(val, index)
-		hashTable.m[priority] = val
+		HashTable.m[priority] = val
 	}
+}
+
+// GetFromTable struct defined
+func GetFromTable(priority int) int {
+	var result int
+	var x int
+
+	if val, exist := HashTable.m[priority]; exist {
+		if len(val) == 1 {
+			result = val[0]
+			val = nil
+		} else {
+			x, val = val[0], val[1:]
+			HashTable.m[priority] = val
+			result = x
+		}
+	}
+	return result
 }
